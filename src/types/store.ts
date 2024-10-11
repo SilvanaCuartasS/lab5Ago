0f30811243704c68efa539da431557ba51f53548
SilvanaCuartas <165075107+SilvanaCuartasS@users.noreply.github.com>
2024-10-11 16:38:33 -0500

    Lab

---
+++ src/types/store.ts
@@ -0,0 +1,14 @@
+export type AppState = {
+	screen: string;
+	shoppingList: any;
+};
+
+export type Observer = { render: () => void } & HTMLElement;
+
+export enum Actions {
+	'ADDPRODUCT' = 'ADDPRODUCT',
+}
+
+export enum Screens {
+	'DASHBOARD' = 'DASHBOARD',
+}
